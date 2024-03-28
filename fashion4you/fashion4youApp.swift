5c72e7173c8319aa879d5f5458179bd6abc1e33b
AshanXT <109849631+AshanXT@users.noreply.github.com>
2024-03-28 14:36:13 +0530

    Initial Commit

---
+++ fashion4you/fashion4youApp.swift
@@ -0,0 +1,17 @@
+//
+//  fashion4youApp.swift
+//  fashion4you
+//
+//  Created by user2 on 2024-03-28.
+//
+
+import SwiftUI
+
+@main
+struct fashion4youApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
