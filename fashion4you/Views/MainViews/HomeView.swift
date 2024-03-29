3d558149b8a2538a3f95c6692dfd99114a78ff82
AshanXT <ashannadeeja48@gmail.com>
2024-03-29 11:41:12 +0530

    home page allignments done

--- fashion4you/Views/MainViews/HomeView.swift
+++ fashion4you/Views/MainViews/HomeView.swift
@@ -12,18 +12,89 @@ struct HomeView: View {
         ZStack{
             Color.white.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             ZStack {
-                
+                VStack(spacing: 0){
+                    
+                    ScrollView(.vertical){
+                        
+                        HeroImageView()
+                        NewArrivalView()
+                    }
+                    .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    
+                }
+                .padding(.top, 56)
                 
             }
-            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-            .overlay {
+            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
+            .overlay(alignment: .top) {
                 
-                
-                
+                HeaderView {
+                    
+                } cartAction: {
+                    
+                }
             }
             
         }
     }
+    
+    @ViewBuilder
+    private func HeroImageView() -> some View  {
+        ZStack{
+            Image("Hero")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(maxWidth: .infinity)
+                .frame(height: 620)
+            
+            Button{
+                print("Explore Collection Button Clicked")
+            } label: {
+                RoundedRectangle(cornerRadius: 30).overlay {
+                Text("Explore Collection")
+                        .font(Font.custom("TenorSans-Regular", size:20))
+                        .foregroundColor(.white)
+                        
+                
+                }
+               
+               
+            }
+            .frame(width: 253, height: 40)
+            .tint(.black.opacity(0.4))
+            .offset(.init(width: 0, height: 250))
+            
+        }
+        
+        
+    }
+    
+    @ViewBuilder
+    private func NewArrivalView() -> some View  {
+        
+        
+        
+    }
+    @ViewBuilder
+    private func CollectionsView() -> some View  {
+        
+        
+        
+    }
+    @ViewBuilder
+    private func SideMenu() -> some View  {
+        
+        
+        
+    }
+    
+    @ViewBuilder
+    private func SideCart() -> some View  {
+        
+        
+        
+    }
+    
 }
 
 #Preview {
